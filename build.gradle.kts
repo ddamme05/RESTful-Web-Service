4bf9b4e8df3c988aa1dc5e11ba090e8c93de0e1f
ddamme05 <ddamme05@gmail.com>
2024-01-14 19:33:47 -0500

    gradle.build.kts modified to use Spring Boot and starter dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.2.1"
 }
+
+apply(plugin = "io.spring.dependency-management")
 
 group = "org.ddamme"
 version = "1.0-SNAPSHOT"
@@ -9,9 +12,17 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.test {
