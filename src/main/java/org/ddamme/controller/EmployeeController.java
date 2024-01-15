abe677d593a5eb62c393f4d55bfc9ff48d2b8ec1
ddamme05 <ddamme05@gmail.com>
2024-01-14 21:04:55 -0500

    Modified EmployeeController to only focus as a route controller.

--- src/main/java/org/ddamme/controller/EmployeeController.java
+++ src/main/java/org/ddamme/controller/EmployeeController.java
@@ -1,26 +1,15 @@
 package org.ddamme.controller;
 
-import org.ddamme.model.Employee;
+import org.ddamme.service.EmployeeManager;
+import org.ddamme.model.Employees;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 @RestController
 public class EmployeeController {
-    private static final List<Employee> EMPLOYEES = createEmployeeList();
-
-    private static List<Employee> createEmployeeList() {
-        List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("1", "John", "Doe", "john.doe@example.com", "Developer"));
-        employees.add(new Employee("2", "Jane", "Doe", "jane.doe@example.com", "Manager"));
-        return employees;
-    }
 
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees() {
-        return EMPLOYEES;
+    public Employees getAllEmployees() {
+        return EmployeeManager.getEmployees();
     }
 }
