84b725fb951a4fb67ff65b4c7d17e38eb5491bbd
ddamme05 <ddamme05@gmail.com>
2024-01-14 21:00:58 -0500

    Running curl for GET request works.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    //implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.test {
